3a5c0a3ed28c9e497608ef6d6ff8336a06a6bad4
alhanson7210 <alhanson@dons.usfca.edu>
2021-05-23 03:36:44 -0700

    Added mock routes for riff tracker

--- src/app/app.py
+++ src/app/app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 def hello():
-    return 'Hello from the other side of docker!'
+    if request.method == "GET":
+        return render_template('home.html')
+    if request.method == "POST":
+        return redirect('/followers')
 
-app.add_url_rule('/', '/', hello)
+def followers():
+    if request.method == "GET":
+        return render_template('data.html')
+
+app.add_url_rule('/', '/', hello, methods=['GET', 'POST'])
+app.add_url_rule('/followers', 'followers', followers, methods=['GET'])
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
